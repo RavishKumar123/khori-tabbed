11990a7e0dace11d90e12ddfda44af9e6ea9ef2c
Ravish Kumar <ravishkumar@Ravishs-Mac.local>
2018-02-24 06:05:40 -0800

    mini player seperate class

--- tabbed/FirstViewController.swift
+++ tabbed/FirstViewController.swift
@@ -9,52 +9,23 @@
 import UIKit
 import ImageSlideshow
 import Firebase
-import FontAwesome_swift
-import AVFoundation
-
 
 class FirstViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var imageSlideShow: ImageSlideshow!
-    let playerRef = Player()
-    var playerActive = false
-    var isPlaying = false
     var playerXib = Bundle.main.loadNibNamed("Player", owner: self, options: nil)?.first as? Player
-    //var player:AVAudioPlayer!
     var recentBhajanArray = [Recent]()
     let localSource = [ImageSource(imageString: "img1")!, ImageSource(imageString: "img2")!, ImageSource(imageString: "img3")!, ImageSource(imageString: "img4")!]
-    var player:AVPlayer?
-    var playerItem:AVPlayerItem?
-    var playButton:UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         retrieveRecentAddedBhajans()
         imagesliderShow()
-//        let urlstring = "http://radio.spainmedia.es/wp-content/uploads/2015/12/tailtoddle_lo4.mp3"
-//        let url = NSURL(string: urlstring)
-//
-//
-//
-//
-//        do {
-//            player = try AVAudioPlayer(contentsOf: url! as URL)
-//            player.prepareToPlay()
-//
-//            player.play()
-//        } catch let error as NSError {
-//            //self.player = nil
-//            print(error.localizedDescription)
-//        } catch {
-//            print("AVAudioPlayer init failed")
-//        }
-//
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         return recentBhajanArray.count
     }
- 
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recentBhajans", for: indexPath) as! RecentlyBhajansCollectionViewCell
         let user = recentBhajanArray[(recentBhajanArray.count - 1) - indexPath.row]
@@ -62,7 +33,6 @@ class FirstViewController: UIViewController,UICollectionViewDelegate,UICollectio
         cell.recentyBhajanButton.tag = indexPath.row
         return cell
     }
-
     func retrieveRecentAddedBhajans(){
         let ref = Database.database().reference().child("bhajans").queryLimited(toLast: 10)
         ref.observe(DataEventType.value,with: {(snapshot) in
@@ -77,6 +47,7 @@ class FirstViewController: UIViewController,UICollectionViewDelegate,UICollectio
                     self.recentBhajanArray.append(recentBhajansObject)
                     print(self.recentBhajanArray.count)
                     self.collectionView.reloadData()
+                    self.playerXib?.recentBhajanArray = self.recentBhajanArray
                 }
             }
             
@@ -84,124 +55,26 @@ class FirstViewController: UIViewController,UICollectionViewDelegate,UICollectio
        
      }
     func imagesliderShow () {
-        // Do any additional setup after loading the view, typically from a nib.
         imageSlideShow.backgroundColor = UIColor.white
         imageSlideShow.slideshowInterval = 2.0
         imageSlideShow.pageControlPosition = PageControlPosition.insideScrollView
         imageSlideShow.pageControl.currentPageIndicatorTintColor = UIColor.lightGray
         imageSlideShow.pageControl.pageIndicatorTintColor = UIColor.white
         imageSlideShow.contentScaleMode = UIViewContentMode.scaleAspectFill
-        
-        // optional way to show activity indicator during image load (skipping the line will show no activity indicator)
         imageSlideShow.activityIndicator = DefaultActivityIndicator()
         imageSlideShow.currentPageChanged = { page in
-            // print("current page:", page)
         }
-        
-        // can be used with other sample sources as `afNetworkingSource`, `alamofireSource` or `sdWebImageSource` or `kingfisherSource`
         imageSlideShow.setImageInputs(localSource)
-        
-        //        let recognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap))
-        //        slideshow.addGestureRecognizer(recognizer)
     }
-    func displayPlayerXib(bhajanIndex:Int){
+    @IBAction func show(_ sender: UIButton) {
         playerXib?.close()
         playerXib = Bundle.main.loadNibNamed("Player", owner: self, options: nil)?.first as? Player
         playerXib?.frame = CGRect(x: 0, y: self.view.frame.height - 100, width: self.view.frame.width, height: 50)
-        playerXib?.c.titleLabel?.font = UIFont.fontAwesome(ofSize: 30)
-        playerXib?.c.setTitle(String.fontAwesomeIcon(name: .close), for: .normal)
-        playerXib?.play.titleLabel?.font = UIFont.fontAwesome(ofSize: 30)
-        playerXib?.play.setTitle(String.fontAwesomeIcon(name: .pause), for: .normal)
-            let user = recentBhajanArray[(recentBhajanArray.count - 1) - bhajanIndex]
-        playerXib?.songName.text = user.bhajanName
-        playerXib?.play.addTarget(self, action: #selector(self.playPause(_:)), for: .touchUpInside);
-        playerXib?.c.addTarget(self, action: #selector(self.close(_:)), for: .touchUpInside);
-        playBhajan(bhajanUrl: user.bhajanUrl!)
-        isPlaying = true
+        playerXib?.recentBhajanArray = recentBhajanArray
+        playerXib?.displayPlayerXib(bhajanIndex: sender.tag)
         self.navigationController?.view.addSubview(playerXib!)
-        
     }
-    @objc func close(_ sender : UIButton){
-        if let play = player {
-            print("stopped")
-            play.pause()
-            player = nil
-            print("player deallocated")
-        } else {
-            print("player was already deallocated")
-        }
-        playerXib?.close()
-    }
-    @objc func playPause(_ sender : UIButton){
-        isPlaying = !isPlaying
-        if(isPlaying){
-        player?.play()
-        playerXib?.play.setTitle(String.fontAwesomeIcon(name: .pause), for: .normal)
-        }else {
-            player?.pause()
-            playerXib?.play.setTitle(String.fontAwesomeIcon(name: .play), for: .normal)
-        }
-        
-    }
-    @IBAction func show(_ sender: UIButton) {
-        
-        displayPlayerXib(bhajanIndex: sender.tag)
-        
-    }
-    func playBhajan(bhajanUrl:String){
-        player?.pause()
-        let url = URL(string: bhajanUrl)
-        let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
-        player = AVPlayer(playerItem: playerItem)
-        player?.play()
-        
-    }
-    func Playpause(){
-        player?.pause()
-    }
-    
-    
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//
-//        let url = URL(string: "https://firebasestorage.googleapis.com/v0/b/khori-3a84d.appspot.com/o/bhajans%2FBeat%20Pe%20Booty.mp3?alt=media&token=1e2bb54a-46c4-4f61-9959-aa4931ee0f81")
-//        let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
-//        player = AVPlayer(playerItem: playerItem)
-//
-//        let playerLayer=AVPlayerLayer(player: player!)
-//        playerLayer.frame=CGRect(x:0, y:0, width:10, height:50)
-//        self.view.layer.addSublayer(playerLayer)
-//
-//        playButton = UIButton(type: UIButtonType.system) as UIButton
-//        let xPostion:CGFloat = 50
-//        let yPostion:CGFloat = 100
-//        let buttonWidth:CGFloat = 150
-//        let buttonHeight:CGFloat = 45
-//
-//        playButton!.frame = CGRect(x: xPostion, y: yPostion, width: buttonWidth, height: buttonHeight)
-//        playButton!.backgroundColor = UIColor.lightGray
-//        playButton!.setTitle("Play", for: UIControlState.normal)
-//        playButton!.tintColor = UIColor.black
-//        playButton!.addTarget(self, action: #selector(FirstViewController.playButtonTapped(_:)), for: .touchUpInside)
-//
-//        self.view.addSubview(playButton!)
-//    }
-//
-//    @objc func playButtonTapped(_ sender:UIButton)
-//    {
-//        if player?.rate == 0
-//        {
-//            player!.play()
-//            //playButton!.setImage(UIImage(named: "player_control_pause_50px.png"), forState: UIControlState.Normal)
-//            playButton!.setTitle("Pause", for: UIControlState.normal)
-//        } else {
-//            player!.pause()
-//            //playButton!.setImage(UIImage(named: "player_control_play_50px.png"), forState: UIControlState.Normal)
-//            playButton!.setTitle("Play", for: UIControlState.normal)
-//        }
-//    }
-//
-    
 }
+
 
 
--- tabbed/Player.swift
+++ tabbed/Player.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import FontAwesome_swift
+import AVFoundation
 
 class Player: UIView {
 
@@ -18,23 +19,56 @@ class Player: UIView {
         // Drawing code
     }
     */
-  
+    var isPlaying = false
+    var recentBhajanArray = [Recent]()
+    var player:AVPlayer?
+    var playerItem:AVPlayerItem?
     @IBOutlet weak var songName: UILabel!
     @IBOutlet weak var play: UIButton!
     @IBOutlet weak var c: UIButton!
     @IBAction func closeAction(_ sender: Any) {
-    self.isHidden = true
+    close()
+    }
+    @IBAction func playAction(_ sender: Any) {
+        playPause()
+    }
+    func returnCount() -> Int {
+        return recentBhajanArray.count
+    }
+    func displayPlayerXib(bhajanIndex:Int){
+        c.titleLabel?.font = UIFont.fontAwesome(ofSize: 30)
+        c.setTitle(String.fontAwesomeIcon(name: .close), for: .normal)
+        play.titleLabel?.font = UIFont.fontAwesome(ofSize: 30)
+        play.setTitle(String.fontAwesomeIcon(name: .pause), for: .normal)
+        let user = recentBhajanArray[(recentBhajanArray.count - 1) - bhajanIndex]
+        //playerXib?.songName.text = user.bhajanName
+        songName.text = user.bhajanName
+        playBhajan(bhajanUrl: user.bhajanUrl!)
     }
     func close(){
+        player = nil
         self.isHidden = true
     }
-
-    @IBAction func playAction(_ sender: Any) {
-        let f1 = FirstViewController()
-        f1.Playpause()
+    func playPause(){
+        isPlaying = !isPlaying
+        if(isPlaying){
+            player?.pause()
+            play.setTitle(String.fontAwesomeIcon(name: .play), for: .normal)
+        }else {
+            player?.play()
+            play.setTitle(String.fontAwesomeIcon(name: .pause), for: .normal)
+        }
+        
         
     }
+    func playBhajan(bhajanUrl:String){
+        let url = URL(string: bhajanUrl)
+        let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
+        player = AVPlayer(playerItem: playerItem)
+        player?.play()
     
+    }
     
     
 }
+
